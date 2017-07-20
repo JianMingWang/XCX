f8a7403fec31d7aa56e2c3370250892b8dfa34c4
JianMingWang <1249331134@qq.com>
2017-07-20 23:49:05 +0800

    0720修改

--- code/app.js
+++ code/app.js
@@ -1,49 +1,48 @@
-const openIdUrl = require('./config').openIdUrl
-
+//app.js
 App({
-  onLaunch: function () {
-    console.log('App Launch')
+  onLaunch: function() {
+    //调用API从本地缓存中获取数据
+    var logs = wx.getStorageSync('logs') || []
+    logs.unshift(Date.now())
+    wx.setStorageSync('logs', logs)
   },
-  onShow: function () {
-    console.log('App Show')
+
+  getUserInfo: function(cb) {
+    var that = this
+    if (this.globalData.userInfo) {
+      typeof cb == "function" && cb(this.globalData.userInfo)
+    } else {
+      //调用登录接口
+      wx.getUserInfo({
+        withCredentials: false,
+        success: function(res) {
+          that.globalData.userInfo = res.userInfo
+          typeof cb == "function" && cb(that.globalData.userInfo)
+        }
+      })
+    }
   },
-  onHide: function () {
-    console.log('App Hide')
-  },
-  globalData: {
-    hasLogin: false,
-    openid: null
-  },
-  // lazy loading openid
-  getUserOpenId: function(callback) {
+
+  getUserOpenId: function (callback) {
     var self = this
 
     if (self.globalData.openid) {
       callback(null, self.globalData.openid)
     } else {
       wx.login({
-        success: function(data) {
-          wx.request({
-            url: openIdUrl,
-            data: {
-              code: data.code
-            },
-            success: function(res) {
-              console.log('拉取openid成功', res)
-              self.globalData.openid = res.data.openid
-              callback(null, self.globalData.openid)
-            },
-            fail: function(res) {
-              console.log('拉取用户openid失败，将无法正常使用开放接口等服务', res)
-              callback(res)
-            }
-          })
+        success: function (data) {
+          console.log(data)
         },
-        fail: function(err) {
+        fail: function (err) {
           console.log('wx.login 接口调用失败，将无法正常使用开放接口等服务', err)
           callback(err)
         }
       })
     }
+  },
+
+  globalData: {
+    userInfo: null,
+    openid: null
   }
 })
